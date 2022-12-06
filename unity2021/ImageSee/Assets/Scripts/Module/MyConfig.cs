0b7100b74b7c2dc6befa474b97413f736c488231
Easlee Liu <easlee@outlook.com>
2022-12-06 22:44:16 +0800

    generated

--- unity2021/ImageSee/Assets/Scripts/Module/MyConfig.cs
+++ unity2021/ImageSee/Assets/Scripts/Module/MyConfig.cs
@@ -8,10 +8,37 @@ namespace XTC.FMP.MOD.ImageSee.LIB.Unity
     /// </summary>
     public class MyConfig : MyConfigBase
     {
+
+        public class Pending
+        {
+            [XmlAttribute("image")]
+            public string image { get; set; } = "";
+        }
+
+        public class Background
+        {
+            [XmlAttribute("visible")]
+            public bool visible { get; set; } = true;
+            [XmlAttribute("color")]
+            public string color { get; set; } = "#00000000";
+        }
+
+        public class ToolBar
+        {
+            [XmlElement("Anchor")]
+            public Anchor anchor { get; set; } = new Anchor();
+        }
+
         public class Style
         {
             [XmlAttribute("name")]
             public string name { get; set; } = "";
+            [XmlElement("Background")]
+            public Background background { get; set; } = new Background();
+            [XmlElement("Pending")]
+            public Pending pending { get; set; } = new Pending();
+            [XmlElement("ToolBar")]
+            public ToolBar toolBar { get; set; } = new ToolBar();
         }
 
 
--- unity2021/ImageSee/Assets/Scripts/Module/MyInstance.cs
+++ unity2021/ImageSee/Assets/Scripts/Module/MyInstance.cs
@@ -1,12 +1,8 @@
-
-
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 using LibMVCS = XTC.FMP.LIB.MVCS;
-using XTC.FMP.MOD.ImageSee.LIB.Proto;
-using XTC.FMP.MOD.ImageSee.LIB.MVCS;
+using System.IO;
 
 namespace XTC.FMP.MOD.ImageSee.LIB.Unity
 {
@@ -15,6 +11,20 @@ namespace XTC.FMP.MOD.ImageSee.LIB.Unity
     /// </summary>
     public class MyInstance : MyInstanceBase
     {
+        public class UiReference
+        {
+            public Image background;
+            public RawImage image;
+            public Transform tfToolBar;
+            public RawImage pending;
+            public Button btnZoomOut;
+            public Button btnZoomIn;
+        }
+
+        private ContentReader contentReader_ = null;
+        private UiReference uiReference_ = new UiReference();
+        private float scale_ = 1f;
+        private Vector2 originSizeDelta_;
 
         public MyInstance(string _uid, string _style, MyConfig _config, MyCatalog _catalog, LibMVCS.Logger _logger, Dictionary<string, LibMVCS.Any> _settings, MyEntryBase _entry, MonoBehaviour _mono, GameObject _rootAttachments)
             : base(_uid, _style, _config, _catalog, _logger, _settings, _entry, _mono, _rootAttachments)
@@ -29,6 +39,15 @@ namespace XTC.FMP.MOD.ImageSee.LIB.Unity
         /// </remarks>
         public void HandleCreated()
         {
+            uiReference_.background = rootUI.transform.Find("bg").GetComponent<Image>();
+            uiReference_.image = rootUI.transform.Find("ScrollView/Viewport/Image").GetComponent<RawImage>();
+            uiReference_.pending = rootUI.transform.Find("Pending").GetComponent<RawImage>();
+            uiReference_.tfToolBar = rootUI.transform.Find("ToolBar");
+            uiReference_.btnZoomOut = rootUI.transform.Find("ToolBar/btnZoomOut").GetComponent<Button>();
+            uiReference_.btnZoomIn = rootUI.transform.Find("ToolBar/btnZoomIn").GetComponent<Button>();
+
+            applyStyle();
+            bindEvents();
         }
 
         /// <summary>
@@ -46,7 +65,13 @@ namespace XTC.FMP.MOD.ImageSee.LIB.Unity
         /// </remarks>
         public void HandleOpened(string _source, string _uri)
         {
+            contentReader_ = new ContentReader(contentObjectsPool);
+            contentReader_.AssetRootPath = settings_["path.assets"].AsString();
+            uiReference_.image.gameObject.SetActive(false);
             rootUI.gameObject.SetActive(true);
+            uiReference_.pending.gameObject.SetActive(true);
+            uiReference_.tfToolBar.gameObject.SetActive(false);
+            loadImage(_source, _uri);
         }
 
         /// <summary>
@@ -55,6 +80,90 @@ namespace XTC.FMP.MOD.ImageSee.LIB.Unity
         public void HandleClosed()
         {
             rootUI.gameObject.SetActive(false);
+            contentReader_ = null;
+        }
+
+        private void loadImage(string _source, string _uri)
+        {
+            string contentUri = Path.GetDirectoryName(_uri);
+            string file = Path.GetFileName(_uri);
+            contentReader_.ContentUri = contentUri;
+            contentReader_.LoadTexture(file, (_texture) =>
+            {
+                uiReference_.image.texture = _texture;
+                uiReference_.image.SetNativeSize();
+                fitImage();
+                uiReference_.pending.gameObject.SetActive(false);
+                uiReference_.image.gameObject.SetActive(true);
+                uiReference_.tfToolBar.gameObject.SetActive(true);
+                scale_ = 1.0f;
+            }, () =>
+            {
+
+            });
+        }
+
+        private void fitImage()
+        {
+            var rtParent = uiReference_.image.transform.parent.GetComponent<RectTransform>();
+            var rtImage = uiReference_.image.rectTransform;
+            //图片和容器的宽度差值
+            float differenceX = rtImage.rect.size.x - rtParent.rect.size.x;
+            //图片和容器的高度差值
+            float differenceY = rtImage.rect.size.y - rtParent.rect.size.y;
+            float fitWidth = rtImage.rect.width;
+            float fitHeight = rtImage.rect.height;
+            if (differenceX > 0 || differenceY > 0)
+            {
+                if (differenceX > differenceY)
+                {
+                    fitWidth = rtParent.rect.size.x;
+                    fitHeight = rtImage.rect.size.y / rtImage.rect.size.x * fitWidth;
+                }
+                else
+                {
+                    fitHeight = rtParent.rect.size.y;
+                    fitWidth = rtImage.rect.size.x / rtImage.rect.size.y * fitHeight;
+                }
+            }
+            originSizeDelta_ = new Vector2(fitWidth, fitHeight);
+            rtImage.sizeDelta = originSizeDelta_;
+        }
+
+        private void applyStyle()
+        {
+            uiReference_.background.gameObject.SetActive(style_.background.visible);
+            Color color;
+            ColorUtility.TryParseHtmlString(style_.background.color, out color);
+            uiReference_.background.color = color;
+
+            loadTextureFromTheme(style_.pending.image, (_texture) =>
+            {
+                uiReference_.pending.texture = _texture;
+                uiReference_.pending.SetNativeSize();
+            }, () =>
+            {
+
+            });
+
+            alignByAncor(uiReference_.tfToolBar, style_.toolBar.anchor);
+        }
+
+        private void bindEvents()
+        {
+            uiReference_.btnZoomIn.onClick.AddListener(() =>
+            {
+                scale_ += 0.2f;
+                uiReference_.image.rectTransform.sizeDelta = originSizeDelta_ * scale_;
+
+            });
+            uiReference_.btnZoomOut.onClick.AddListener(() =>
+            {
+                scale_ -= 0.2f;
+                if (scale_ < 1)
+                    scale_ = 1;
+                uiReference_.image.rectTransform.sizeDelta = originSizeDelta_ * scale_;
+            });
         }
     }
 }
